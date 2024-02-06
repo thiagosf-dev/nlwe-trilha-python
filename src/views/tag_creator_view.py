a3cf36f47075f3d7f5fe46bce831d1a014660393
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2024-02-06 20:40:58 -0300

    FEAT: Implementing the controller for /create_tag route

--- src/views/tag_creator_view.py
+++ src/views/tag_creator_view.py
@@ -1,12 +1,15 @@
+from src.controllers.tag_creator_controller import TagCreatorController
 from src.views.http_types.http_request import HttpRequest
 from src.views.http_types.http_response import HttpResponse
 
 
 class TagCreatorView:
     def validate_and_create(self, http_request: HttpRequest) -> HttpResponse:
+        tag_creator_controller = TagCreatorController()
         body = http_request.body
         product_code = body["product_code"]
+        response = tag_creator_controller.create(product_code)
         return HttpResponse(
             status_code=200,
-            body={"code": product_code}
+            body=response
         )
