edccdcbee2083462cd39c39881c73d71f132d2d8
Thiago Soares Figueiredo <thiagosf.dev@gmail.com>
2024-02-08 01:23:42 -0300

    FEAT: Implementing Validator

--- src/errors/error_handler.py
+++ src/errors/error_handler.py
@@ -1,7 +1,18 @@
+from src.errors.error_types.http_unprocessable_entity import HttpUnprocessableEntityError
 from src.views.http_types.http_response import HttpResponse
 
 
 def handle_errors(error: Exception) -> HttpResponse:
+    if isinstance(error, HttpUnprocessableEntityError):
+        return HttpResponse(
+            status_code=error.status_code,
+            body={
+                "errors": [{
+                    "title": error.name,
+                    "detail": error.message
+                }]
+            }
+        )
     return HttpResponse(
         status_code=500,
         body={
--- src/main/routes/tag_routes.py
+++ src/main/routes/tag_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, jsonify, request
 from src.errors.error_handler import handle_errors
+from src.validators.tag_creator_validator import tag_creator_validator
 
 from src.views.http_types.http_request import HttpRequest
 from src.views.tag_creator_view import TagCreatorView
@@ -12,6 +13,7 @@ tags_routes_bp = Blueprint('tags_routes', __name__)
 def create_tag():
     response = None
     try:
+        tag_creator_validator(request)
         tag_creator_view = TagCreatorView()
         http_request = HttpRequest(body=request.json)
         response = tag_creator_view.validate_and_create(http_request)
